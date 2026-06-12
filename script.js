a6818f52bb3ffcfdd6290fb3cd7784de50daa3e9
marianapacheco4-dev <maripacheco0804@gmail.com>
2026-06-12 11:13:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let imagens = [
-    "imagens/IMG_261.jpeg",
-    "imagens/IMG_262.jpeg",
-    "imagens/IMG_263.jpeg",
-    "imagens/IMG_264.jpeg",
-    "imagens/IMG_265.jpeg",
-    "imagens/IMG_266.jpeg",
-    "imagens/IMG_268.jpeg",
-    "imagens/IMG_269.jpeg"
+    "IMG_261.jpeg",
+    "IMG_262.jpeg",
+    "IMG_263.jpeg",
+    "IMG_264.jpeg",
+    "IMG_265.jpeg",
+    "IMG_266.jpeg",
+    "IMG_268.jpeg",
+    "IMG_269.jpeg"
 ];
 
 let index = 0;
